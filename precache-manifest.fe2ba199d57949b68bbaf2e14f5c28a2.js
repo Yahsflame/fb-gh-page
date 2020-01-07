dcc99f56da814b0b2cdcd458495b211f4e521a4a
Ben Sigal <bensigal@rocketmail.com>
2020-01-06 17:01:23 -0800

    Updates

---
+++ precache-manifest.fe2ba199d57949b68bbaf2e14f5c28a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "968a81d17212f5001651d10e8feb33c6",
+    "url": "/fb-gh-page/index.html"
+  },
+  {
+    "revision": "0c0745cb2d9ef8067572",
+    "url": "/fb-gh-page/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "93e7a0400b08b989557c",
+    "url": "/fb-gh-page/static/js/2.17d9efa1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/fb-gh-page/static/js/2.17d9efa1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0c0745cb2d9ef8067572",
+    "url": "/fb-gh-page/static/js/main.194ef228.chunk.js"
+  },
+  {
+    "revision": "26ba2ddb812619c3b7fb",
+    "url": "/fb-gh-page/static/js/runtime-main.e0e4f126.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fb-gh-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
